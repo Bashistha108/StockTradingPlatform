b4ee74bb846784f2513858ceacc151a1dfc71324
Bashistha Joshi <bashsithajoshi108@gmail.com>
2024-10-24 01:55:28 +0200

    database connected to spring boot successfully

--- tradingapp/tradingapp/sql_scripts/create_user.sql
+++ tradingapp/tradingapp/sql_scripts/create_user.sql
@@ -1,6 +1,6 @@
 
 /* Creating a user with name stock_trading_app */
-DROP USER IF EXISTS 'stock_trading_app'@'localhost';
+DROP USER IF EXISTS `stock_trading_app`@`localhost`;
 /* Identified by ..... to set password */
-CREATE USER 'stock_trading_app'@'localhost' IDENTIFIED BY 'stock_trading_app';
-GRANT ALL PRIVILEGES ON * . * TO 'stock_trading_app';
+CREATE USER `stock_trading_app`@`localhost` IDENTIFIED BY 'stock_trading_app';
+GRANT ALL PRIVILEGES ON  `stock_trading_app`.* TO `stock_trading_app`@`localhost`;

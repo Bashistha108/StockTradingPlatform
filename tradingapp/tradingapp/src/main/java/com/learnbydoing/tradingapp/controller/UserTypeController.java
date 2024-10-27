852b97dd6e19fc72515ef82de0944cd26b455f1a
Bashistha Joshi <bashsithajoshi108@gmail.com>
2024-10-27 02:57:45 +0200

    User and UserType Controller created but not working completely

---
+++ tradingapp/tradingapp/src/main/java/com/learnbydoing/tradingapp/controller/UserTypeController.java
@@ -0,0 +1,35 @@
+package com.learnbydoing.tradingapp.controller;
+
+import com.learnbydoing.tradingapp.entity.UserType;
+import com.learnbydoing.tradingapp.service.UserTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user-types")
+public class UserTypeController {
+
+    private UserTypeService userTypeService;
+
+    @Autowired
+    public UserTypeController(UserTypeService userTypeService){
+        this.userTypeService = userTypeService;
+    }
+
+    @GetMapping
+    public List<UserType> getAllUserTypes(){
+        return userTypeService.getAllUserTypes();
+    }
+
+    @PostMapping("/create")
+    public UserType createUserType(@RequestBody UserType userType){
+        return userTypeService.createUserType(userType);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUserType(@PathVariable int id){
+        userTypeService.deleteUserType(id);
+    }
+}
